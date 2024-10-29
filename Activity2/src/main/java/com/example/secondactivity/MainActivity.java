c21a9e008b10588fae2fb04a9042b3d673035114
Husna Zahira <153571648+zahirarzli@users.noreply.github.com>
2024-10-30 01:37:28 +0800

    Task1

---
+++ Activity2/src/main/java/com/example/secondactivity/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.secondactivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button buttonNavigate = findViewById(R.id.buttonNavigate);
+        buttonNavigate.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+
+               Toast.makeText(MainActivity.this, "Button was clicked!", Toast.LENGTH_SHORT).show();
+
+               Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+               intent.putExtra("EXTRA_MESSAGE", "Hello from MainActivity!");
+               startActivity(intent);
+           }
+        });
+    }
+}
---
+++ Activity2/src/main/java/com/example/secondactivity/SecondActivity.java
@@ -0,0 +1,29 @@
+package com.example.secondactivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        String message = getIntent().getStringExtra("EXTRA_MESSAGE");
+        TextView textView = findViewById(R.id.textViewSecond);
+        textView.setText(message);
+
+        Button buttonBack = findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Closes the current activity and returns to the previous one
+            }
+        });
+    }
+}
