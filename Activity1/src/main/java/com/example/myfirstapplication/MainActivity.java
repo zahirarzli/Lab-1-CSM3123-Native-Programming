628f57200d68d337d85e2c41fb7c3d78457ff609
Husna Zahira <153571648+zahirarzli@users.noreply.github.com>
2024-10-30 01:36:44 +0800

    Task1

---
+++ Activity1/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myfirstapplication;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Button Clicked!");
+                textView.setTextColor(Color.RED);
+            }
+        });
+    }
+}
